31771113500c9dff21f9925dcb7ec39a62d225f0
Ehsan Pazhooman <ehspazh@gmail.com>
2025-02-28 19:37:38 +0330

    Develop shared domain for DDD

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,9 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+
+
+var builder = WebApplication.CreateBuilder(args);  //The builder is used as the foundation for configuring your application's services before the app starts.
 
 // Add services to the container
+
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
+
+app
+    .UseBasketModule()
+    .UseOrderingModule()
+    .UseCatalogModule();
+
 
 app.Run();
---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog
+{
+    /// <summary>
+    /// This class will be registering all dependencies regarding the catalog module
+    /// </summary>
+    public static class CatalogModule
+    {
+        public static IServiceCollection AddCatalogModule(this IServiceCollection services,IConfiguration configuration)
+        {
+            // Add services to the container
+            // services
+            //      .AddApplicationServices()
+            //      .AddInfrastructureServices(configuration)
+            //      .AddApiServices(configuration);
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
+        {
+            // Configure the HTTP request pipeline
+            //app
+            //    .UseApplicationServices()
+            //    .UseInfrastructureServices()
+            //    .UseApiServices()
+            return app;
+        }
+    }
+}
